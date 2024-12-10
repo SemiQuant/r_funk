68a6b4075ac59557b369309b2335d5cd7c7d943f
SemiQuant <lmbjas002@myuct.ac.za>
2024-12-09 20:34:53 -0800

    updating

--- R/data.R
+++ R/data.R
@@ -1,11 +1,7 @@
 #' Gene Signatures Dataset
 #'
-#' A dataset containing gene signatures for various pathways and conditions
+#' A dataset containing gene signatures information
 #'
-#' @format A data frame with columns:
-#'   \describe{
-#'     \item{Symbol}{Gene symbol}
-#'     \item{ID}{Signature identifier}
-#'   }
-#' @source Internal collection of gene signatures
-"gene_signatures" 
+#' @format A data frame containing gene signatures
+#' @source Internal gene signatures compilation
+"gene_signatures"
--- data-raw/gene_signatures.R
+++ data-raw/gene_signatures.R
@@ -1,5 +1,8 @@
 # Script to prepare gene signatures dataset
 gene_signatures <- readxl::read_excel("data-raw/gene_signatures.xlsx")
 
+# Clean up any potential issues (NA values, etc)
+gene_signatures <- as.data.frame(gene_signatures)
+
 # Save as internal data
-usethis::use_data(gene_signatures, internal = FALSE, overwrite = TRUE) 
+usethis::use_data(gene_signatures, internal = FALSE, overwrite = TRUE)
